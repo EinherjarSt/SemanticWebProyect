ff69b273edfc758f9fbea96cc2f3537c1a56e93d
EinherjarSt <gsanhueza15@alumnos.utalca.cl>
2019-09-20 21:21:24 -0300

    Parse oscar

---
+++ src/main/java/my/company/websemanticaoscar/App.java
@@ -0,0 +1,49 @@
+package my.company.websemanticaoscar;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+import org.apache.jena.vocabulary.RDFS;
+import org.apache.jena.vocabulary.RDFSyntax;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	parseOscars();
+    }
+    
+    public static void parseOscars () {
+    	RDFSParser rdf = new RDFSParser();
+    	System.out.println(System.getProperty("user.dir"));
+    	try(BufferedReader reader = new BufferedReader(new FileReader("dataset/data_csv.csv"))){
+    		String line;
+    		reader.readLine();
+    		while ((line = reader.readLine()) != null) {
+    			String[] tokens = line.split(";");
+    			rdf.parseTokens(tokens);
+    			//System.out.println(rdf.convertNameToResourceName(tokens[3]));
+    		}
+    		rdf.printModel();
+    		rdf.saveModelToFile(new File("output/oscar.ttl"));
+    	}
+    	catch (IOException e) {
+    		e.printStackTrace();
+		}
+    }
+    
+    public static void printTokens(String[] tokens) {
+    	for (String string : tokens) {
+    		System.out.println(string);
+			//System.out.print(string + " | ");
+		}
+    	System.out.println(tokens.length);
+    	System.out.println();
+    }
+}
---
+++ src/main/java/my/company/websemanticaoscar/RDFSParser.java
@@ -0,0 +1,62 @@
+package my.company.websemanticaoscar;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+
+import org.apache.jena.ontology.ObjectProperty;
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.rdf.model.Property;
+import org.apache.jena.rdf.model.Resource;
+import org.apache.jena.vocabulary.RDF;
+import org.apache.jena.vocabulary.RDFS;
+import org.apache.jena.vocabulary.RDFSyntax;
+
+public class RDFSParser {
+	Model model;
+	String voc = "http://oscars.org/voc#";
+	String movieURL = "http://oscars.org/";
+	private Property yearProperty;
+	private Property categoryProperty;
+	private Property veredictProperty;
+	private Property nomineeProperty;
+	
+	public RDFSParser() {
+		this.model = ModelFactory.createDefaultModel();
+		model.setNsPrefix("voc", voc);
+		yearProperty = model.createProperty(voc,"year");
+		categoryProperty = model.createProperty(voc,"category");
+		veredictProperty = model.createProperty(voc,"veredict");
+		nomineeProperty = model.createProperty(voc,"nominee");
+	}
+	
+	public void parseTokens(String[] tokens) {
+		Resource subject = model.createResource(movieURL+convertNameToResourceName(tokens[3]));
+		Resource classRdfs = model.createResource(voc+"Nominee");
+		subject.addProperty(RDF.type, classRdfs);
+		subject.addProperty(yearProperty, tokens[0]);
+		subject.addProperty(categoryProperty, tokens[1]);
+		subject.addProperty(veredictProperty, tokens[2]);
+		subject.addProperty(nomineeProperty, tokens[3]);
+		
+	}
+	
+	public String convertNameToResourceName(String string) {
+		return string.replace(" ", "_").replace(",", "").replace(".", "");
+		
+	}
+	
+	public void saveModelToFile(File file) {
+		try {
+			this.model.write(new FileOutputStream(file), "Turtle");
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public void printModel() {
+		this.model.write(System.out, "Turtle");
+	}
+}
